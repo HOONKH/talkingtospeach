555b9013598457bf6cddfb791280ad47c3598dc2
HOONKH <boospa0305@gmai.com>
2023-11-29 16:39:54 +0900

    woodysry

--- realtts/src/pages/day.jsx
+++ realtts/src/pages/day.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import englishData from "../englishData.json";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 // englishData.map((v) => console.log(v));
 // v.day
@@ -8,6 +9,7 @@ const Day = () => {
   const [dailyData, setDailyData] = useState();
   const [isVisible, setIsVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const { day } = useParams();
   // day
 
@@ -24,6 +26,56 @@ const Day = () => {
   };
   // 다음과 이전 구현 참고 !!!!!!!
   // *********************************************************
+  const onClickSound = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isLoading) return;
+      const response = await axios.post(
+        `https://texttospeech.googleapis.com/v1/text:synthesize?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          input: {
+            text: dailyData.sentences[currentPage].english,
+          },
+          voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+          audioConfig: {
+            audioEncoding: "MP3",
+            speakingRate: 0.8,
+            pitch: -15.0,
+          },
+        }
+      );
+      console.log(response);
+      const binaryData = atob(response.data.audioContent);
+
+      const byteArray = new Uint8Array(binaryData.length);
+      // 형태를 변화시켜준다 Uint8Array 생성 => 바이너리데이터 길이만큼에
+      for (let i = 0; i < binaryData.length; i++) {
+        // console.log(binaryData.charCodeAt(i));
+        // charcodAt의 역할
+        byteArray[i] = binaryData.charCodeAt(i);
+        // 자름과 동시에 바이트 단위에 배열을 생성 변환을 해서 바이트어래이에 저장
+      }
+
+      const blob = new Blob([byteArray.buffer], { type: "audio/mp3" });
+      // 블롭으로만드는 자바스크립트형태
+      // console.log(binaryData);
+      // console.log(blob); 블롭형태
+
+      const newAudio = new Audio(URL.createObjectURL(blob));
+
+      // console.log(newAudio); html 코드가 들어옴
+
+      document.body.appendChild(newAudio);
+      newAudio.play();
+
+      setTimeout(() => setIsLoading(false), 3000);
+    } catch (error) {
+      console.log(error);
+
+      setIsLoading(false);
+    }
+  };
   useEffect(() => {
     englishData.forEach((v) => {
       if (v.day === +day) {
@@ -46,46 +98,50 @@ const Day = () => {
   // useState의 초기값이 0이기떄문에 렌더링시 로딩
 
   return (
-    <div className="container relative">
+    <div className="container relative bg-[#f2f2f2c7]">
       <div className="absolute top-0 left-0 p-8">
         <Link
           to="/"
-          className="btn ml-1 lazy text-xl font-black px-1 shadow-lg hover:bg-slate-200"
+          className="btn ml-1 lazy text-xl font-black text-green-600 px-1 shadow-lg hover:bg-slate-200"
         >
           Baaack
         </Link>
       </div>
-      <h1 className="lazy text-4xl font-black">
+      <h1 className="lazy text-5xl font-black">
         Day {dailyData.day} - {dailyData.title}
       </h1>
       <div className="mt-12">
-        <dic className="lazy text-3xl">
+        <div className="lazy text-3xl">
           {dailyData.sentences[currentPage].english}
-        </dic>
+        </div>
         <button
-          className={`text-xs ${!isVisible && "bg-black"}`}
+          className={`text-xs shadow-md ${!isVisible && "bg-black"}`}
           onClick={() => setIsVisible(!isVisible)}
         >
           {dailyData.sentences[currentPage].korean}
         </button>
-        <div className="mt-3">
+        <div className="mt-10">
           <button
             onClick={onClickPrev}
-            className="border-2 lazy border-slate-200 rounded-md text-xl px-2 shadow-lg hover:bg-slate-200"
+            className="border-2 lazy border-slate-200 rounded-md text-xl px-2 shadow-lg font-black hover:bg-slate-200"
           >
             Prev
           </button>
           <button
-            className="btn ml-2 lazy text-xl px-2 shadow-lg hover:bg-slate-200"
+            className="btn ml-2 lazy text-xl px-2 shadow-lg font-black hover:bg-slate-200"
             onClick={onClickNext}
           >
             Next
           </button>
-          <button className="btn ml-2 lazy text-xl px-2 shadow-lg hover:bg-slate-200">
+          <button
+            onClick={onClickSound}
+            className="btn ml-2 lazy text-xl px-2 shadow-lg font-black text-blue-700 hover:bg-slate-200"
+          >
             Sound
           </button>
         </div>
       </div>
+      <img className="ml-[380px] mt-[145px]" src="./images/woody.svg" alt="" />
     </div>
   );
 };
--- realtts/src/pages/main.jsx
+++ realtts/src/pages/main.jsx
@@ -2,18 +2,22 @@ import MainCard from "../components/MainCard";
 import englishData from "../englishData.json";
 
 const Main = () => {
-  console.log(englishData);
-
+  // console.log(englishData); JSON 잘들어왔는지 확인
   return (
-    <div className="container">
-      <div className="flex flex-col gap-8">
-        <h1 className="text-4xl font-[lazyday] font-black">Study English</h1>
+    <div className="container bg-[#f2f2f2c7]">
+      <div className="flex flex-col gap-8 ">
+        <h1 className="text-5xl font-[lazyday] font-black">Study English</h1>
         {englishData.map((v, i) => (
-          <ul className="ease-out  hover:bg-slate-200 duration-700 rounded-md">
+          <ul
+            className={`ease-out  hover:bg-slate-200 duration-700 rounded-md ${
+              v.day % 2 ? "text-black" : "text-green-600"
+            }`}
+          >
             <MainCard key={i} title={v.title} day={v.day} />
           </ul>
         ))}
       </div>
+      <img className="ml-[380px]" src="./images/woody.svg" alt="" />
     </div>
   );
 };
